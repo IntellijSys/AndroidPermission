b4a589ae4bcc66b16eaeca34ef801dbb95fd18fb
Mohammad Nurdin (Zero) <mohammadnrdn@gmail.com>
2017-05-19 16:55:10 +0800

    Update MainActivity.java.

--- app/src/main/java/my/intellij/androidpermission/MainActivity.java
+++ app/src/main/java/my/intellij/androidpermission/MainActivity.java
@@ -1,13 +1,109 @@
 package my.intellij.androidpermission;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.RECORD_AUDIO;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button check_permission = (Button) findViewById(R.id.check_permission);
+        Button request_permission = (Button) findViewById(R.id.request_permission);
+        check_permission.setOnClickListener(this);
+        request_permission.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        view = v;
+
+        int id = v.getId();
+        switch (id) {
+            case R.id.check_permission:
+                if (checkPermission()) {
+
+                    Snackbar.make(view, "Permission already granted.", Snackbar.LENGTH_LONG).show();
+
+                } else {
+
+                    Snackbar.make(view, "Please request permission.", Snackbar.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.request_permission:
+                if (!checkPermission()) {
+
+                    requestPermission();
+
+                } else {
+
+                    Snackbar.make(view, "Permission already granted.", Snackbar.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
+
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{RECORD_AUDIO, CAMERA}, PERMISSION_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+
+                    boolean recordAudioAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean cameraAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (recordAudioAccepted && cameraAccepted)
+                        Snackbar.make(view, "Permission Granted, Now you can access audio data and camera.", Snackbar.LENGTH_LONG).show();
+                    else {
+
+                        Snackbar.make(view, "Permission Denied, You cannot access audio data and camera.", Snackbar.LENGTH_LONG).show();
+                    }
+                }
+
+
+                break;
+        }
+    }
+
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 }
